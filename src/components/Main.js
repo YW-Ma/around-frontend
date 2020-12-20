ddefb47bcaa76ed0d99ff00d2f91f328ce75b22c
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-12-21 06:49:00 +0800

    configured React-Router

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,18 @@ import React from 'react';
 import Login from "./Login";
 import Register from "./Register";
 import Home from "./Home";
+import {Switch, Route} from "react-router-dom"
 
 function Main(props) {
   return (
-    <div>
-      Main
+    // Route负责路由切换
+    // Switch负责只显示一个完全匹配项
+    <div className="main">
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/login/register" component={Register}/>
+        <Route path="/home" component={Home}/>
+      </Switch>
     </div>
   );
 }
